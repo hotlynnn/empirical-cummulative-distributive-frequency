b0f15bcb81da0af6b5be2c9e6db00ed8c43d6eca
Franklin Adjei <laizer421@gmail.com>
2020-12-10 14:06:46 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='ecdf',
+    version='0.0.1',
+    description='A Distribution Function Associated With The Empirical Measure Of A Sample.',
+    url = 'https://github.com/Hotlynn2'
+    author='Mr. Franklin Adjei',
+    author_email='laizer421@gmail.com',
+    license='MIT',
+    packages=['ecdf'],
+    install_requires=['numpy', 'matplotlib'],
+    include_package_data=True
+)
+ 
