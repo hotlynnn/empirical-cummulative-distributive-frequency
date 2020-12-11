f38efa4326b14c331380a19b904521130b24b433
Franklin Adjei <laizer421@gmail.com>
2020-12-11 11:47:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='ECDF',
-    version='0.0.3',
+setup(name='empirical_cdf',
+    version='0.0.1',
     description='A Distribution Function Associated With The Empirical Measure Of A Sample.',
     url = 'https://github.com/Hotlynn2',
     author='Mr. Franklin Adjei',
     author_email='laizer421@gmail.com',
     license='MIT',
-    packages=['ECDF'],
+    packages=['empirical_cdf'],
     install_requires=['numpy', 'matplotlib'],
     include_package_data=True
 )
